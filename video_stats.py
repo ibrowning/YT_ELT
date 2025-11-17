ccc79ef1137dbbed0053d6b44d719d7578a08f85
Isaiah <Isaiahbrowning34@yahoo.com>
2025-11-16 20:13:06 -0500

    Added extracted data function to retrieve the video id then created a dictionary

--- video_stats.py
+++ video_stats.py
@@ -122,7 +122,7 @@ def extract_video_data(video_ids):
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    
+    extract_video_data(video_ids)
 
     
 
